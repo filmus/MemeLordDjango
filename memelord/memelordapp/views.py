667cbe1d4b44faa788df56c17cee6f0c16c3ce2b
unknown <filip.musiol.96@gmail.com>
2020-02-11 00:16:11 +0100

    Initial commit with main page basic functionality

---
+++ memelord/memelordapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'memelordapp/home.html', context)
